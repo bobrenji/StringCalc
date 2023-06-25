2378e17e820a4c4dfbc1046bbbb7470ddd2a62ca
Renji <renji129@gmail.com>
2023-06-25 15:10:30 +0800

    Add a spec with requirements

---
+++ src/StringCalculator.spec.ts
@@ -0,0 +1,6 @@
+describe('StringCalculator', function () {
+    it.todo('when more than 2 numbers are used then throw exception')
+    it.todo('when two numbers are used then no exception is thrown')
+    it.todo('when non numbers are used then throw exception')
+    
+});
