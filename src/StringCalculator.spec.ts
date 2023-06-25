d92ef4db1dfe0326a4334c1e3316a86c7da25030
Renji <renji129@gmail.com>
2023-06-25 16:20:41 +0800

    Scenario 1: When more than 2 numbers passed then should throw exception

--- src/StringCalculator.spec.ts
+++ src/StringCalculator.spec.ts
@@ -1,6 +1,12 @@
+import StringCalculator from "./StringCalculator";
+
 describe('StringCalculator', function () {
     it.todo('when more than 2 numbers are used then throw exception')
     it.todo('when two numbers are used then no exception is thrown')
     it.todo('when non numbers are used then throw exception')
-    
+
+    it('when more than 2 numbers are used then throw exception', function () {
+        let stringCalc = new StringCalculator();
+        expect(() => stringCalc.add('1,2,3')).toThrowError();
+    });
 });
--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,3 +1,10 @@
-class StringCalculator {
+export default class StringCalculator {
 
+    add(numberString: String): number {
+        let length = numberString.length
+        if(length>2) {
+            throw Error('More than two numbers')
+        }
+        return 0
+    }
 }
